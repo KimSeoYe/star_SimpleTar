a021b0d8704f7413a37c8017364baf9d0af99646
KimSeoYe <kimseoye15@gmail.com>
2021-06-30 11:54:48 +0900

    11:54 complete archive() and extract() w/ a single file

--- star.c
+++ star.c
@@ -104,20 +104,25 @@ get_parameters (int argc, char ** argv, char * option, char * star_file, char * 
 /*
     1개의 파일에 대해 동작하는 프로그램 만들기
     1. archive 인 경우
-        * header 구조체 선언
-        * stat 으로 star_dir 의 정보를 읽는다 (type_id, data_size) -> header 구조체에 저장한다
-            * directory일 경우 length가 0이어야 한다!!
-        * star_dir의 이름의 크기를 header 구조체에 저장한다.
-        * return header...
-        * star_file 이라는 이름으로 새로운 파일을 연다. (write)
-        * header structure 의 정보를 쓴다
-        
-        * star_dir 파일의 이름을 쓴다
-        * directory면 contents를 쓰지 않는다...
-        * -> directory가 아니면
-            * star_dir 파일을 연다 (read)
-            * star_dir 의 내용을 읽으며 그 뒤에 바로 이어 쓴다.
-        * closedir, fclose, ...
+        # 디렉토리를 먼저 열어야 한다
+            # 그 다음 하위 파일을 archive 해야한다...
+
+        # 디렉토리 열기
+        # 하위 내용 읽기
+            * header 구조체 선언 
+            * stat 으로 star_dir 의 정보를 읽는다 (type_id, data_size) -> header 구조체에 저장한다
+                * directory일 경우 length가 0이어야 한다!!
+            * star_dir의 이름의 크기를 header 구조체에 저장한다.
+            * return header...
+            * star_file 이라는 이름으로 새로운 파일을 연다. (write)
+            * header structure 의 정보를 쓴다
+            
+            * star_dir 파일의 이름을 쓴다
+            * directory면 contents를 쓰지 않는다...
+            * -> directory가 아니면
+                * star_dir 파일을 연다 (read)
+                * star_dir 의 내용을 읽으며 그 뒤에 바로 이어 쓴다.
+            * closedir, fclose, ...
         
         Q. 읽으면서 바로 write 하는게 나을까? 아니면 파일을 읽는 과정과 쓰는 과정을 분리하는게 좋을까? -> 읽으면서 write 해보자
         Q. 파일 이름의 크기를 따로 저장해 둬야 할 것 같다 
@@ -144,17 +149,15 @@ get_parameters (int argc, char ** argv, char * option, char * star_file, char * 
 
 ///////////////////////////////////////////// archive /////////////////////////////////////////////
 
-Header *
-make_header(char * dir)  
+void
+make_header(char * dir, Header * h)  
 {
-    Header * h = (Header *) malloc(sizeof(Header)) ;
+    struct stat st ;
+    stat(dir, &st) ;
     
-    struct stat * st ;
-    stat(dir, st) ;
+    h->data_size = st.st_size ;
     
-    h->data_size = st->st_size ;
-    
-    if (S_ISDIR(st->st_mode)) {
+    if (S_ISDIR(st.st_mode)) {
         h->type_id = 0 ;
     }
     else {  /* Q. any other case..? */
@@ -164,14 +167,12 @@ make_header(char * dir)
     h->name_size = strlen(dir) ;
 
     strcpy(h->path_name, dir) ;
-
-    return h ;
 }
 
 void
 write_header_data(FILE * w_fp, Header * h)
 {
-     if (fwrite(&h->type_id, 1, sizeof(h->type_id), w_fp) != sizeof(h->type_id)) 
+    if (fwrite(&h->type_id, 1, sizeof(h->type_id), w_fp) != sizeof(h->type_id)) 
         goto err_exit ;
     if (fwrite(&h->name_size, 1, sizeof(h->name_size), w_fp) != sizeof(h->name_size)) 
         goto err_exit ;
@@ -187,98 +188,122 @@ err_exit:
     exit(1) ;
 }
 
+void read_and_write (FILE * r_fp, FILE * w_fp, size_t size) {
+    int s ;
+    for(s = 0; s < size; ) {
+        char buffer[512] ;
+        size_t r_len = fread(buffer, 1, sizeof(buffer), r_fp) ;
+        if (r_len != fwrite(buffer, 1, r_len, w_fp)) {
+            // do something?
+        }
+        s += r_len ;
+    }       
+}
+
+
 void
-write_contents_data (FILE * w_fp, Header * h, char * target_path)
+write_data (FILE * w_fp, Header * h, char * source)
 {
+    if (fwrite(&h->type_id, 1, sizeof(h->type_id), w_fp) != sizeof(h->type_id)) 
+        goto err_exit ;
+    if (fwrite(&h->name_size, 1, sizeof(h->name_size), w_fp) != sizeof(h->name_size)) 
+        goto err_exit ;
+    if (fwrite(&h->data_size, 1, sizeof(h->data_size), w_fp) != sizeof(h->data_size)) 
+        goto err_exit ;
+    if (fwrite(h->path_name, 1, h->name_size, w_fp) != h->name_size) 
+        goto err_exit ;
+
     if (h->type_id == 1) {   // not a directory!
 
-        FILE * r_fp = fopen(target_path , "rb") ;
+        FILE * r_fp = fopen(source , "rb") ;
         if (r_fp == NULL) {
-            perror("ERROR: fopen - target_path") ; 
+            perror("ERROR: fopen") ; 
             exit(1) ;
         }
 
-        size_t r_len ;
-        while (feof(r_fp) == 0) {
-            char buffer[512];
-            r_len = fread(buffer, 1, sizeof(buffer), r_fp) ;
-            if (r_len != fwrite(buffer, 1, r_len, w_fp)) {
-                perror("ERROR: fwrite - file contetns") ;
-                exit(1) ;
-            }
-        }       
+        read_and_write(r_fp, w_fp, h->data_size) ;
+
         fclose(r_fp) ;
     }
+    
+    return ;
+
+err_exit:
+    perror("Error: fail to write a header data") ;
+    exit(1) ;
+}
+
+/*
+    디렉토리 안에 있는 파일을 archive 해야 한다!
+
+*/
+
+
+char *
+get_path (char * dir_path, char * sub_dir)
+{
+    char * path = (char*) malloc(sizeof(char) * PATH_MAX);
+    path = strdup(dir_path);
+    strcat(path, "/");
+    strcat(path, sub_dir);
+    return path;
 }
 
 void
 archive (char * star_file, char * star_dir) 
 {
-    Header * head = make_header(star_dir) ;
-    
-    FILE * w_fp ;
-    w_fp = fopen(star_file, "wb") ;
-    if(w_fp == NULL) {
-        perror("ERROR: Failed to open a file") ;
+    DIR * dp = opendir(star_dir) ;
+    if(dp == NULL) {
+        perror("ERROR: opendir") ;
         exit(1) ;
     }
+    else {
+        struct dirent * sub ;
+        for( ; sub = readdir(dp) ; ) {
+            if (strcmp(sub->d_name, "..") != 0 && strcmp(sub->d_name, ".") != 0) {
+                Header head ;
+                make_header(sub->d_name, &head) ;
+                
+                FILE * w_fp ;
+                w_fp = fopen(star_file, "wb") ;
+                if(w_fp == NULL) {
+                    perror("ERROR: Failed to open a file") ;
+                    exit(1) ;
+                }
 
-    write_header_data(w_fp, head) ;
-    write_contents_data(w_fp, head, star_dir) ;
+                char * src_path = get_path(star_dir, sub->d_name) ;
+                write_data(w_fp, &head, src_path) ;
 
-    free(head) ;
-    rewind(w_fp) ;
-    fclose(w_fp) ;
+                fclose(w_fp) ;
+            }            
+        } 
+    }
 }
 
 ///////////////////////////////////////////// extract /////////////////////////////////////////////
 // 헤더 구조에 맞춰 바꿔야 함!
 
-Header *
-read_header (FILE * r_fp)
-{
-    Header * h = (Header *) malloc(sizeof(Header)) ;
-
-    if (sizeof(h->type_id) != fread(&h->type_id, 1, sizeof(h->type_id), r_fp)) {
-        perror("ERROR: fread - h->type_id") ;
-        exit(1) ;
-    }
-    if (sizeof(h->name_size) != fread(&h->name_size, 1, sizeof(h->name_size), r_fp)) {
-        perror("ERROR: fread - h->name_size") ;
-        exit(1) ;
-    }
-    if (sizeof(h->data_size) != fread(&h->data_size, 1, sizeof(h->data_size), r_fp)) {
-        perror("ERROR: fread - h->data_size") ;
-        exit(1) ;
-    }
-    if (h->name_size != fread(h->path_name, 1, h->name_size, r_fp)) {
-        perror("ERROR: fread - new file_name @open_new_file()") ;
-        exit(1) ;
-    }
-
-    return h ;
-}
-
-FILE *
-open_new_file (FILE * r_fp, Header * h)
-{
-
-    FILE * n_fp = fopen(h->path_name, "wb") ;
-    if (n_fp == NULL) {
-        perror("ERROR: fopen - @open_new_file()") ; 
-        exit(1) ;
-    }
-
-    return n_fp ;
-}
-
 void
-write_new_file(FILE * w_fp, FILE * r_fp, Header * h) 
+read_header (FILE * r_fp, Header * h)
 {
-    // * h->data_size 만큼의 파일내용의 크기를 읽는다
-    // *** buffer size를 정해놓고 연속적으로 읽어야 함
-    // * 읽어낸 크기 만큼의 파일 내용을 읽으며 새로 열린 파일에 쓴다.
+    // Header * h = (Header *) malloc(sizeof(Header)) ;
+
+    if (sizeof(h->type_id) != fread(&h->type_id, 1, sizeof(h->type_id), r_fp)) 
+        goto err_exit ;
+    if (sizeof(h->name_size) != fread(&h->name_size, 1, sizeof(h->name_size), r_fp)) 
+        goto err_exit ;
+    if (sizeof(h->data_size) != fread(&h->data_size, 1, sizeof(h->data_size), r_fp)) 
+        goto err_exit ;
+    if (h->name_size != fread(h->path_name, 1, h->name_size, r_fp)) 
+        goto err_exit ;
+
+    return ;
+
+err_exit:
+    perror("Error: fail to read a header data") ;
+    exit(1) ;
 }
+
 
 void
 extract (char * star_file)
@@ -289,14 +314,25 @@ extract (char * star_file)
         exit(1) ;
     }
 
-    Header * head = read_header(r_fp) ;
-    
-    FILE * n_fp = open_new_file(r_fp, head) ;
-    write_new_file(n_fp, r_fp, head) ;
+    Header head ;
+    read_header(r_fp, &head) ;
 
-
-    free(head) ;
-    fclose(n_fp) ;
+    if (head.type_id == 0) {
+        if (mkdir(head.path_name, 0766) == -1) {
+            perror("ERROR: mkdir") ;
+            exit(1) ;
+        }
+    } 
+    if (head.type_id == 1) { 
+        FILE * w_fp = fopen(head.path_name, "wb") ;
+        if (w_fp == NULL) {
+            perror("ERROR: fopen") ; 
+            exit(1) ;
+        }
+        
+        read_and_write(r_fp, w_fp, head.data_size) ;
+    }
+    fclose(r_fp) ;
 }
 
 ///////////////////////////////////////////// list /////////////////////////////////////////////
